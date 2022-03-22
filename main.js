3e3fc453e42e8cf0a773da25c03518f3f7ea057a
Padraic <high5developer@gmail.com>
2022-03-22 10:04:39 +0000

    about section

---
+++ main.js
@@ -0,0 +1,9 @@
+var readMore = document.querySelector(".readmore");
+var readMoreContent = document.querySelector(".read-more-content");
+
+
+console.log(readMoreContent);
+
+console.log("hellow");
+
+readMore.addEventListener("click", function() { alert('hi')});
