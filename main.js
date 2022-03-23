9d64aa2877d55b3013d960f40bb4e7fd6cce2eb8
Padraic <high5developer@gmail.com>
2022-03-23 10:42:21 +0000

    projects section

--- main.js
+++ main.js
@@ -1,9 +1,55 @@
-var readMore = document.querySelector(".readmore");
-var readMoreContent = document.querySelector(".read-more-content");
+const menuBtn =document.querySelector('#menu-btn');
+const closeBtn = document.querySelector('#close-btn');
+const menu = document.querySelector('nav .container ul');
+const navItems = menu.querySelectorAll('li');
 
 
-console.log(readMoreContent);
 
-console.log("hellow");
+menuBtn.addEventListener('click', () => {
+    menu.style.display = "block";
+    menuBtn.style.display = "none";
+    closeBtn.style.display ='inline-block';
+})
 
-readMore.addEventListener("click", function() { alert('hi')});
+closeBtn.addEventListener('click', () => {
+    menu.style.display = "none";
+    menuBtn.style.display = "inline-block";
+    closeBtn.style.display ='none';
+});
+
+const changeActive = () => {
+   navItems.forEach(item =>{
+        const link = item.querySelector('a');
+        link.classList.remove('active');
+   })
+}
+
+navItems.forEach(item => {
+    const link = item.querySelector('a');
+    link.addEventListener('click', () => {
+        changeActive();
+        link.classList.add('active');
+    });
+})
+
+const readMore = document.querySelector('.read-more');
+const readLess = document.querySelector('.read-less');
+const read = document.querySelector('.read-more-content');
+
+readMore.addEventListener('click', ()=> {
+    read.style.display = "block";
+    readMore.style.display = "none";
+    readLess.style.display = "block";
+    
+
+});
+
+readLess.addEventListener('click', () => {
+    read.style.display = "none";
+    readMore.style.display = "block";
+    readLess.style.display = "none";
+});
+
+window.addEventListener('scroll',() => {
+    document.querySelector('nav').classList.toggle('show-box-shadow',window.scrollY > 0)
+})
